0888726e5d66b5cc18e547ec126e58ac7fdde710
himn1 <zbigniew.humeniuk@gmail.com>
2017-10-28 01:05:53 +0200

    webpack + babel

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log("loco-js-model");
+const Loco = {};
---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+  entry: './index.js',
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'loco-js-model.js'
+  }
+};

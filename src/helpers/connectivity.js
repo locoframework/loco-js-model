0282c76c07f09aa3bb1bebf09f2404cf9624d970
Zbigniew Humeniuk <zbigniew.humeniuk@gmail.com>
2018-01-31 00:07:14 +0100

    eslint, prettier etc

--- src/helpers/connectivity.js
+++ src/helpers/connectivity.js
@@ -1,12 +1,13 @@
-export const filterParams = (data) => {
+export const filterParams = data => {
   const params = {};
-  if(!data) return params;
-  for(const prop in data) {
-    if(prop === "resource") continue;
-    params[prop] = data[prop];
-  }
+  if (!data) return params;
+  Object.keys(data).forEach(prop => {
+    if (prop !== "resource") {
+      params[prop] = data[prop];
+    }
+  });
   return params;
-}
+};
 
 export const sendReq = (httpMeth, url, data) => {
   const meta = document.querySelector("meta[name='csrf-token']");
@@ -14,9 +15,9 @@ export const sendReq = (httpMeth, url, data) => {
   req.open(httpMeth, url);
   req.setRequestHeader("Accept", "application/json");
   req.setRequestHeader("Content-Type", "application/json");
-  if(meta) {
+  if (meta) {
     req.setRequestHeader("X-CSRF-Token", meta.content);
   }
   req.send(JSON.stringify(data));
   return req;
-}
+};
